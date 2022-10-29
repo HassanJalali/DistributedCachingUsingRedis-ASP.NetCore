73e0f4fef9e9a8074cb6893629b8ac4ce1d5b40d
Hassan Jalali <mister.developer@yahoo.com>
2022-10-29 21:31:09 +0330

    Preparing For Distributed Caching

---
+++ Distributed Caching/Distributed Caching/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using Distributed_Caching.Data;
+using Distributed_Caching.Extensions;
+using Distributed_Caching.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Distributed_Caching.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : Controller
+    {
+        private readonly DbContextClass _context;
+        private readonly IDistributedCache _cache;
+        private readonly ILogger<Product> _logger;
+        private static readonly SemaphoreSlim semaphore = new(1, 1);
+
+        public ProductController(DbContextClass context, IDistributedCache cache, ILogger<Product> logger)
+        {
+            _context = context;
+            _cache = cache ?? throw new ArgumentNullException(nameof(cache));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        //[HttpGet]
+        //public async Task<IActionResult> GetAsync()
+        //{
+        //    var products = _context.Products.ToList();
+        //    _logger.Log(LogLevel.Information, "Trying to fetch the list of Products from cache.");
+
+        //    if (_cache.TryGetValue(products, out IEnumerable<Product>? Products))
+        //    {
+
+        //    }
+        //}
+
+    }
+}
---
+++ Distributed Caching/Distributed Caching/Extensions/DistributedCache.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Distributed_Caching.Extensions
+{
+    public static class DistributedCache
+    {
+        public static Task SetAsync<T>(this IDistributedCache cache, string key, T value)
+        {
+            return SetAsync(cache, key, value, new DistributedCacheEntryOptions());
+        }
+
+        public static Task SetAsync<T>(this IDistributedCache cache, string key, T value, DistributedCacheEntryOptions options)
+        {
+            var bytes = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(value, GetJsonSerializerOptions()));
+            return cache.SetAsync(key, bytes, options);
+        }
+
+
+        //TryGetValue() generic method gets the value from the cache as a byte array and converts it into the respective type.
+        public static bool TryGetValue<T>(this IDistributedCache cache, string key, out T? value)
+        {
+            var val = cache.Get(key);
+            value = default;
+            if (val == null) return false;
+
+            value = JsonSerializer.Deserialize<T>(val, GetJsonSerializerOptions());
+            return true;
+        }
+
+
+        //GetJsonSerializerOptions()  is a helper method for initializing the JSON serializer options.
+        private static JsonSerializerOptions GetJsonSerializerOptions()
+        {
+            return new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = null,
+                WriteIndented = true,
+                AllowTrailingCommas = true,
+                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+            };
+        }
+    }
+}
